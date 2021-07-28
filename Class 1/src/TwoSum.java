501d863ebbf4f57de47518cf14ae07857746d7d9
Sakib <fahmidhossainsakib@gmail.com>
2021-07-29 00:59:39 +0600

    3 lines of Comment added inside twoSum method !

--- Class 1/src/TwoSum.java
+++ Class 1/src/TwoSum.java
@@ -23,6 +23,9 @@ class TwoSum {
       for(int i=0; i<nums.length-1;i++) {	
     	  for(int j=1; j<=nums.length-1;j++) {
     		  if(nums[i]+nums[j] == target && i != j) {
+    			  // && condition added so that i and j value are never the same
+    			  // ie : in case of nums[] = [2,5,5,11] and target = 10 
+    			  // result[] = [1,1], which is a wrong answer
     			  resultArray[0]= i;
     			  resultArray[1]= j;
     			  break outerloop;
