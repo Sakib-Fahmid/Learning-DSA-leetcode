8ee535b2a12451d6afbb4b8956e203ca04dc87c0
Sakib <fahmidhossainsakib@gmail.com>
2021-07-29 00:35:24 +0600

    Solution of Leetcode's TwoSUm problem added with O(n^2) complexity

---
+++ Class 1/src/TwoSum.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+class TwoSum {
+    public static void main(String[] args){
+      Scanner scanner =new Scanner(System.in);
+      System.out.println("Please enter array size !");
+      int arrayLength = scanner.nextInt();
+      int numbers [] =new int [arrayLength];
+      System.out.println("Please enter array elements !");
+      for(int i=0;i<=arrayLength-1;i++){
+        numbers[i]=scanner.nextInt();
+      }
+      System.out.println("Please enter target value !");
+      int target = scanner.nextInt();
+        TwoSum obj = new TwoSum();
+        int [] result = obj.twoSum(numbers,target);
+        System.out.println("Target value found in :"+Arrays.toString(result));
+    }
+    public int[] twoSum(int[] nums, int target) {
+      
+      int resultArray [] = new int [2];
+      outerloop :
+      for(int i=0; i<nums.length-1;i++) {	
+    	  for(int j=1; j<=nums.length-1;j++) {
+    		  if(nums[i]+nums[j] == target && i != j) {
+    			  resultArray[0]= i;
+    			  resultArray[1]= j;
+    			  break outerloop;
+    		  }
+    	  }
+      }
+      return resultArray;
+    }
+}
